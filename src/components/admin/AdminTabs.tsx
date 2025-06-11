80884e2e26a56e7122567a36d1a6f05523aa278a
João Pedro Murad Panizzutti <joaopanizzutti@gmail.com>
2025-06-11 17:42:04 +0100

    A

--- src/components/admin/AdminTabs.tsx
+++ src/components/admin/AdminTabs.tsx
@@ -5,13 +5,15 @@ import SuggestionManagement from "./SuggestionManagement";
 import ReviewModeration from "./ReviewModeration";
 import SecurityAuditLog from "./SecurityAuditLog";
 import AdminRoleManagement from "./AdminRoleManagement";
+import BulkExpertReviewImport from "./BulkExpertReviewImport";
 
 const AdminTabs = () => {
   return (
     <Tabs defaultValue="influencers" className="w-full">
-      <TabsList className="grid grid-cols-6 w-full">
+      <TabsList className="grid grid-cols-7 w-full">
         <TabsTrigger value="influencers">Influencers</TabsTrigger>
         <TabsTrigger value="bulk-import">Bulk Import</TabsTrigger>
+        <TabsTrigger value="bulk-expert-reviews">Bulk Expert Reviews</TabsTrigger>
         <TabsTrigger value="suggestions">Suggestions</TabsTrigger>
         <TabsTrigger value="reviews">Reviews</TabsTrigger>
         <TabsTrigger value="users">Users</TabsTrigger>
@@ -22,6 +24,9 @@ const AdminTabs = () => {
       </TabsContent>
       <TabsContent value="bulk-import">
         <BulkInfluencerImport />
+      </TabsContent>
+      <TabsContent value="bulk-expert-reviews">
+        <BulkExpertReviewImport />
       </TabsContent>
       <TabsContent value="suggestions">
         <SuggestionManagement />
