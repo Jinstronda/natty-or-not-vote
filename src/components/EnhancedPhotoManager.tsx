3329feb1254ef2f4fe9b1556b0fc8bbcab7979b8
João Pedro Murad Panizzutti <joaopanizzutti@gmail.com>
2025-06-30 15:17:44 +0100

    Update EnhancedPhotoManager.tsx

--- src/components/EnhancedPhotoManager.tsx
+++ src/components/EnhancedPhotoManager.tsx
@@ -285,7 +285,7 @@ export function EnhancedPhotoManager({
       </div>
 
       {/* Photo Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 gap-4">
         {photos.map((photo, index) => (
           <EnhancedPhotoCard
             key={photo.id}
