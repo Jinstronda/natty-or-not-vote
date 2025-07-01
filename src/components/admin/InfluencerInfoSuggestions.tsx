f7d6aa61a0e5dc2e4cd8360ff67321465801ec3d
João Pedro Murad Panizzutti <joaopanizzutti@gmail.com>
2025-07-01 18:59:07 +0100

    Add SuggestInfluencerInfo component and admin placeholder

---
+++ src/components/admin/InfluencerInfoSuggestions.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function InfluencerInfoSuggestions() {
+  return (
+    <div>
+      <h2>Influencer Info Suggestions - Coming Soon</h2>
+    </div>
+  );
+}
